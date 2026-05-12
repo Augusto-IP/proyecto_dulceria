3621b14e98febbf5a193879fd88bf8012c4de22e
Augusto-IP <augustoipushima06@gmail.com>
2026-05-12 14:31:37 -0500

    Commit 3: Implementación de lógica CRUD (Modelo y Controlador) para productos regionales

---
+++ vista/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../activos/css/home.css">
+    <title>home-Dulceria</title>
+</head>
+<body>
+    <aside class="sidebar">
+        <div class="logo">
+            <h1>Doña solida</h1>
+        </div>
+        <nav class="menu">
+            <a href="home.php" class="active"><i class="fas fa-home"></i> Inicio</a>
+            <a href="productos.php"><i class="fas fa-prod ">Productos</a>
+            <a href="usuarios.php"><i class="fas fa-user"></i> Usuarios</a>
+            <a href="logout.php" class="btn-salir"><i class=""fas fa-sign-out-alt">Salir</i></a>
+        </nav>
+    </aside>
+</body>
+</html>
